2597d18b1215bf39e7c0d3cc3fa01dd58317efb9
Taras Pustovyi <taraselnik@gmail.com>
2023-03-06 19:45:39 +0200

    Take out todoitem, select, delete props

--- src/components/StatusFilter.vue
+++ src/components/StatusFilter.vue
@@ -1,12 +1,40 @@
-<script></script>
+<script>
+export default {
+  props: {
+    modelValue: String,
+  },
+  emits: ['update:modelValue'],
+};
+</script>
 
 <template>
   <nav class="filter">
-    <a href="#/" class="filter__link selected"> All </a>
+    <a
+      href="#/"
+      class="filter__link"
+      :class="{selected: modelValue === 'all'}"
+      @click="$emit('update:modelValue', 'all')"
+    >
+      All
+    </a>
 
-    <a href="#/active" class="filter__link"> Active </a>
+    <a
+      href="#/active"
+      class="filter__link"
+      :class="{selected: modelValue === 'active'}"
+      @click="$emit('update:modelValue', 'active')"
+    >
+      Active
+    </a>
 
-    <a href="#/completed" class="filter__link"> Completed </a>
+    <a
+      href="#/completed"
+      class="filter__link"
+      :class="{selected: modelValue === 'completed'}"
+      @click="$emit('update:modelValue', 'completed')"
+    >
+      Completed
+    </a>
   </nav>
 </template>
 
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,63 @@
+<script>
+export default {
+  name: "TodoItem",
+  props: {
+    todo: {
+      type: Object,
+    },
+  },
+  emits: ['update', 'remove'],
+  methods: {
+    toggle() {
+      this.$emit('update', {
+        ...this.todo,
+        completed:!this.todo.completed,
+      });
+    },
+    deleteTodo() {
+      this.$emit('remove');
+    },
+  }
+
+}
+</script>
+
+<template>
+        <div
+          class="todo"
+          :class="{completed: todo.completed}"
+        >
+          <label class="todo__status-label">
+            <input
+              type="checkbox"
+              class="todo__status"
+              :checked="todo.completed"
+              @change="toggle"
+            />
+          </label>
+
+          <form v-if="undefined">
+            <input
+              type="text"
+              class="todo__title-field"
+              placeholder="Empty todo will be deleted"
+              value="Todo is being edited now"
+            />
+          </form>
+
+          <template v-else>
+            <span class="todo__title">
+              {{ todo.title }}</span>
+            <button
+              class="todo__remove"
+              @click="deleteTodo">x</button>
+          </template>
+
+          <div class="modal overlay" :class="{'is-active' : false}">
+            <div class="modal-background has-background-white-ter"></div>
+            <div class="loader"></div>
+          </div>
+        </div>
+</template>
+
+<style></style>
