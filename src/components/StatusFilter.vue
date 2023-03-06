1a2165900ac549ef17433ce6c0b1c586277c0ab1
Taras Pustovyi <taraselnik@gmail.com>
2023-03-06 17:15:53 +0200

    Add localstorage and logic, take out filter to new comp

---
+++ src/components/StatusFilter.vue
@@ -0,0 +1,13 @@
+<script></script>
+
+<template>
+  <nav class="filter">
+    <a href="#/" class="filter__link selected"> All </a>
+
+    <a href="#/active" class="filter__link"> Active </a>
+
+    <a href="#/completed" class="filter__link"> Completed </a>
+  </nav>
+</template>
+
+<style></style>
